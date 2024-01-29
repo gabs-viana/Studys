7f27c2aa85d8b3d4abf08a13c8e33a0fef816b3c
Gabs.dViana <bielmviana@gmail.com>
2024-01-29 17:25:14 -0300

    Remake v1.1b

--- emails.py
+++
@@ -1,6 +0,0 @@
-# email e senha
-email_principal = "bielmviana@gmail.com" 
-# emailbielmviana2@gmail.com
-# settochefe355@gmail.com
-# machado5viana@gmail.com
-# gabriel.machado.viana@escola.pr.gov.br
