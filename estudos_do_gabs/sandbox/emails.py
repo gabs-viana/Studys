9a4e7006619a634d4ce72e06bde90314353d01f5
Gabs.dViana <bielmviana@gmail.com>
2024-01-29 17:41:17 -0300

    Botando ordem na casa

---
+++ estudos_do_gabs/sandbox/emails.py
@@ -0,0 +1,6 @@
+# email e senha
+email_principal = "bielmviana@gmail.com" 
+# emailbielmviana2@gmail.com
+# settochefe355@gmail.com
+# machado5viana@gmail.com
+# gabriel.machado.viana@escola.pr.gov.br
