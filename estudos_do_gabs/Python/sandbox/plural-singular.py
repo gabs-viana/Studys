8f5e2b13888e07a8713e915a69c632d1ec808fcc
Gabs.dViana <bielmviana@gmail.com>
2024-08-27 21:28:41 -0300

    Consertando alteração de nome da pasta

--- estudos_do_gabs/Python/sandbox/plural-singular.py
+++
@@ -1,7 +0,0 @@
-def calc_frase(age, name):
-    if age <= 1:
-        print(f"Meu nome é {name} e tenho {age} ano de idade.")
-    else:
-        print(f"Meu nome é {name} e tenho {age} anos de idade.")
-
-calc_frase(1,"Gabs")
